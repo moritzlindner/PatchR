3879d7b9480a802e6f7b333780f3c30e5a4f8df2
moritzlindner <lindnerm@uni-bonn.de>
2021-01-20 17:28:05 +0100

    IV

--- R/Calculate_IV.R
+++ R/Calculate_IV.R
@@ -20,6 +20,7 @@ setGeneric(name="Calculate_IV",
 #' @param X_FROM,X_TO,x_D_FROM,X_D_TO Time points to perform averaging for IV prodcution
 #' @param ITrace,VTrace Name of the traces containig I and V
 #' @param ReturnPMTRace whether to return results as a PMSeries with an additional, computed trace
+#' @import ggplot2
 #' @return a matrix or PMSeries with IV data and ggplot stored in the MetaData and Plot slot, resp.
 #' @exportMethod Calculate_IV
 setMethod("Calculate_IV",
@@ -33,6 +34,7 @@ setMethod("Calculate_IV",
                    x_D_FROM=NA,
                    X_D_TO=NA){
             out<-SubsetData(object, Time=c(X_FROM,X_TO),nowarnings=T)
+            out<-convenientScales(out)
             out<-apply(object,1,mean)
 
             if(!is.na(X_D_TO)){
@@ -41,22 +43,23 @@ setMethod("Calculate_IV",
               out[,"I.Substracted"]<-out[,ITrace]-substract[,ITrace]
             }
             if(ReturnPMTRace){
-             # object@MetaData[["IV"]]<-out
-print(as.data.frame(out))
-print(VTrace)
-print(ITrace)
-             # object@Plots[["IV"]]<-
-                print(ggplot(as.data.frame(out))+
-                geom_line(aes_string(y=ITrace,x=VTrace))+
-                theme_classic())#+
-                # theme(legend.position = "none",
-                #       text = element_text(size=8))+
-                # xlab(paste("Voltage [",object@Units[getTraces(object)==VTrace],"]"))+
-                # ylab(paste("Current [",object@Units[getTraces(object)==ITrace],"]")))
+              object@MetaData[["IV"]]<-out
+
+              out<-as.data.frame(out)
+              rownames(out)<-NULL
+              colnames(out)[colnames(out)==VTrace]<-"Voltage"
+              colnames(out)[colnames(out)==ITrace]<-"Current"
+              object@Plots[["IV"]]<-ggplot(out)+
+                geom_line(aes(y=Current,x=Voltage))+
+                theme_classic()+
+                theme(legend.position = "none",
+                      text = element_text(size=8))+
+                xlab(paste("Voltage [",object@Units[getTraces(object)==VTrace],"]"))+
+                ylab(paste("Current [",object@Units[getTraces(object)==ITrace],"]"))
 
               if(!is.na(X_D_TO)){
-                object@Plots[["IV"]]<-ggplot(as.data.frame(out))+
-                  geom_line(aes_string(y="I.Substracted",x=VTrace))+
+                object@Plots[["IV.Substracted"]]<-ggplot(as.data.frame(out))+
+                  geom_line(aes_string(y=I.Substracted,x=Voltage))+
                   theme_classic()+
                   theme(legend.position = "none",
                         text = element_text(size=8))+
